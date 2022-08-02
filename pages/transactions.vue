a2988114c81bf58eb4ad1675cb63d80ac694e921
David Emaye <emayeodavid@gmail.com>
2022-08-02 16:03:46 +0100

    developed transactions page

---
+++ pages/transactions.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="container">
+    <p class="page-title">
+      DASHBOARD
+    </p>
+    <hr class="grey-line">
+    <div class="inner">
+      <div class="lhs">
+        <div class="cards">
+          <div class="card">
+            <div class="card-top">
+              <p class="card-name">
+                Present Subscription Plan
+              </p>
+            </div>
+            <div class="card-num">
+              Monthly (₦5,000)
+            </div>
+          </div>
+        </div>
+        <div class="dashboard-table">
+          <TablesTransactionTable />
+        </div>
+      </div>
+      <div class="rhs">
+        <div class="new-magazine">
+          <p class="new-title">
+            Change Subscription Plan
+          </p>
+          <hr>
+          <div class="upload-img">
+            <img src="~assets/img/calendar (1) 1.png" alt="">
+          </div>
+          <p class="click">
+            Click the button to change your Subscription Plan
+          </p>
+          <!-- <p class="pdf-only">
+            (.pdf document only)
+          </p> -->
+          <div class="upload-btn">
+            <button @click="newMagazine = true">
+              Change Plan
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import functions from '@/utils/functions'
+export default {
+  layout: 'MainLayout',
+  data () {
+    return {
+      currency: functions.formatCurrency
+      // cards: [
+      //   {
+      //     name: 'Total Data Uploads',
+      //     number: '524'
+      //   },
+      //   {
+      //     name: 'Total Amount Subscribed',
+      //     number: '45,791'
+      //   },
+      //   {
+      //     name: 'Subscription Plan',
+      //     plan: 'Monthly'
+      //   }
+      // ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 50px 5rem;
+}
+
+.inner {
+  display: flex;
+}
+
+.lhs {
+  flex-basis: 70%;
+  margin-right: 2%;
+}
+
+.rhs {
+  flex-basis: 30%;
+}
+
+.cards {
+  margin-bottom: 40px;
+  display: flex;
+}
+
+.card {
+  background-color: white;
+  flex-basis: 30%;
+  margin-right: 2%;
+  padding: 20px;
+}
+
+.card-top {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+
+.card-num {
+  margin-top: 30px;
+  font-size: 24px;
+  font-weight: 500;
+}
+
+.dashboard-table {
+  width: 100%;
+}
+
+/* RHS  */
+
+.new-magazine {
+  background-color: white;
+  padding: 30px 40px;
+}
+
+.new-title {
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.new {
+  color: #008A27;
+  font-style: italic;
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #E7E8F2;
+  margin: 20px 0;
+}
+
+.upload-img {
+  text-align: center;
+}
+
+.upload-img img {
+  width: 3rem;
+}
+
+.click {
+  font-weight: 500;
+  text-align: center;
+  margin: 12px auto;
+  width: 70%;
+}
+
+.pdf-only {
+  color: #4D4D4D;
+  font-weight: 300;
+  text-align: center;
+}
+
+.upload-btn {
+  text-align: center;
+}
+
+.upload-btn button {
+  cursor: pointer;
+  width: 169px;
+  height: 40px;
+  margin-top: 30px;
+  background: #5C07A3;
+  border-radius: 3px;
+  color: white;
+  border: none;
+  font-size:11px;
+}
+
+@media screen and (max-width: 600px) {
+  .container {
+    padding: 30px;
+  }
+
+  .inner {
+    flex-direction: column;
+  }
+
+  .cards {
+    flex-direction: column;
+  }
+
+  .card {
+    margin-top: 20px;
+  }
+
+  .card-num {
+    font-size: 22px;
+  }
+
+  .card-name {
+    font-size: 15px;
+  }
+
+  .table-top {
+    padding: 0 10px;
+  }
+
+  .rhs {
+    margin-top: 3rem;
+  }
+}
+</style>
